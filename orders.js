d6850aeeebe829b8ad31a56321bdf65415260b01
dtfflex <dtfflex@gmail.com>
2025-06-23 02:20:59 +0600

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,32 @@
+// orders.js
+
+const orders = [
+  {
+    code: "DTF1234",
+    merchantName: "Tanbir",
+    merchantPhone: "017xxxxxxxx",
+    customerName: "Sakib Khan",
+    customerPhone: "018xxxxxxxx",
+    profit: "৳120",
+    product: "Custom T-Shirt",
+    color: "Black",
+    size: "L",
+    status: "In Courier",
+    deliveryNote: "Will arrive in 3 days",
+    estimatedDelivery: "2025-06-25"
+  },
+  {
+    code: "DTF5678",
+    merchantName: "Anika",
+    merchantPhone: "016xxxxxxxx",
+    customerName: "Rafi",
+    customerPhone: "019xxxxxxxx",
+    profit: "৳150",
+    product: "DTF Hoodie",
+    color: "White",
+    size: "M",
+    status: "Delivered",
+    deliveryNote: "Delivered to doorstep",
+    estimatedDelivery: "2025-06-20"
+  }
+];
